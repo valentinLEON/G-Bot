a11eeb0c2c08f63a490052667ab71f96361d6571
Valentin LEON <valentin.leon@tidyuptech.com>
2024-12-16 18:45:11 +0100

    new changes with new sub command

--- commands/utility/gaylord.js
+++ commands/utility/gaylord.js
@@ -5,20 +5,35 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('gaylord')
 		.setDescription('Renvoie le message formaté du nouveau gaylord avec incrémentation et la date')
-		.addUserOption(option => 
-			option
-				.setName('user')
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName('add')
 				.setDescription("Le nom de l'utilisateur")
-				.setRequired(true)
+				.addUserOption(option =>
+					option
+						.setName('user')
+						.setDescription("Le nom de l'utilisateur")
+						.setRequired(true)
+				)
+				.addStringOption(option =>
+					option.setName('message')
+						.setDescription("La citation de l'utilisateur")
+						.setRequired(true)
+				)
 		)
-		.addStringOption(option =>
-			option.setName('message')
-				.setDescription("La citation de l'utilisateur")
-				.setRequired(true)
-		),
-	async execute(interaction, user, message, role) {
-		
-		const stringB = `Le nouveau ${roleMention(role.id)} : ${userMention(user.id)} n°01\n${moment().format("DD/MM/YYYY")} - \"${message}\"`
-		await interaction.reply(stringB);
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName('stats')
+				.setDescription("Renvoie les stats de l'utilisateur sélectionné ou celui qui a eu le plus de gaylord")
+				.addUserOption(option => option.setName('target').setDescription('The user')))
+	,
+	async execute(interaction, user, message, role, count) {
+		if (interaction.options.getSubcommand() === 'stats') {
+			const resStats = `Le plus gros gaylord est ${userMention(user.id)} avec ${count}`;
+			await interaction.reply(resStats);
+		} else {
+			const resGaylord = `Le nouveau ${roleMention(role.id)} : ${userMention(user.id)} n°${count}\n${moment().format("DD/MM/YYYY")} - \"${message}\"`
+			await interaction.reply(resGaylord);
+		}
 	},
 };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Events, GatewayIntentBits, MessageFlags, REST, Routes, Collection } = require('discord.js');
+const { Client, Events, GatewayIntentBits, MessageFlags, REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 const Sequelize = require('sequelize');
@@ -21,6 +21,7 @@ const Users = sequelize.define('tags', {
 		defaultValue: 70,
 		allowNull: false,
 	},
+	date: Sequelize.DATE
 });
 
 // Create a new client instance
@@ -55,12 +56,13 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-		// 
+		// mettre les values dans le .env
 		const data = await rest.put(
 			Routes.applicationCommands('1317068960885379102', '1317067517973364836'),
 			{ body: commands },
 		);
-
+		console.log("Init database");
+		Users.sync();
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
 		console.error(error);
@@ -83,48 +85,96 @@ const getInteraction = (interaction) => {
 	}
 }
 
-
 client.once(Events.ClientReady, async readyClient => {
-	Users.sync();
-	// const initUserInfo = await Users.create({
-	// 	username: 'Tonton Wick',
-	// 	usage_count: 70
-	// });
-	// console.log(`User added : ${initUserInfo.name} with nbr : ${initUserInfo.usage_count}`);
+	// on remplit la base avec les anciens messages
+	const oldMessages = [];
+	const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL);
+
+	console.log("Récupération des anciens messages");
+	const messages = await channel.messages.fetch({ limit: 100 })
+	console.log(`Received ${messages.size} messages`);
+	messages.forEach(message => {
+		if (message.content.includes("n°")) {
+			oldMessages.push(message);
+		}
+	})
+
+	const res = await Users.findAll();
+
+	console.log("Init de la base de données pour stocker les data");
+	if (!res && oldMessages) {
+		console.log("Add all old messages")
+		oldMessages.forEach(async message => {
+			const regex = /@([^@]+) n°/;
+			const match = message.content.match(regex);
+			if (match) {
+				const usernameRegex = match[1];
+				const username = client.users.cache.get(usernameRegex.slice(0, -1)).globalName;
+				const index = message.content.indexOf("n°");
+				const char = "n°";
+				//TODO: maybe find next characters until space...
+				const count = message.content.slice(index + char.length, index + char.length + 3).trim();
+				await Users.create({
+					username: username,
+					usage_count: count
+				})
+			}
+		});
+	}
 	console.log(`Ready! Bot: ${readyClient.user.tag}`);
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
 	const user = interaction.options.getMember('user');
-    const citation = interaction.options.getString('message');
+	const citation = interaction.options.getString('message');
 	const command = getInteraction(interaction);
 
 	const guild = interaction.guild;
 	const role = guild.roles.cache.find(role => role.name === 'Gaylord');
-	
+
 	if (!command) {
 		console.error(`No command matching ${interaction.commandName} was found.`);
 		return;
 	}
+	if (interaction.options._subcommand === 'stats') {
+		const res = await Users.findAll({
+			attributes: [
+				'username',
+				[Sequelize.fn('COUNT', Sequelize.col('username')), 'roleCount']
+			],
+			group: ['username'],
+			limit: 1
+		})
+		console.log("res", res);
+		await command.execute(interaction, "toto", null, null, 0);
+	} else {
+		if (!role) {
+			await interaction.reply("Pas de rôle gaylord existant");
+		}
 
-	if (!role) {
-		await interaction.reply("Pas de rôle gaylord existant");
-	}
+		// suppression du role pour tout le monde avant de le mettre à celui qui le mérite
+		role.members.forEach((member, i) => {
+			member.roles.remove(role);
+		})
 
-	// suppression du role pour tout le monde avant de le mettre à celui qui le mérite
-	role.members.forEach((member, i) => {
-		member.roles.remove(role);
-	})
-	
-	try {
-		await user.roles.add(role);
-		await command.execute(interaction, user, citation, role);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		try {
+			await user.roles.add(role);
+			// get all gaylords and order by usage count asc
+			const res = await Users.findAll({ order: [['usage_count', 'ASC']] });
+			const lastElement = res[res.length - 1];
+			const newUser = await Users.create({
+				username: user.user.globalName,
+				usage_count: lastElement.usage_count + 1
+			});
+			console.log("Add new user");
+			await command.execute(interaction, user, citation, role, newUser.usage_count);
+		} catch (error) {
+			console.error(error);
+			if (interaction.replied || interaction.deferred) {
+				await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+			} else {
+				await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+			}
 		}
 	}
 });
